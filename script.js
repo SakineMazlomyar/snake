1bb150672f87a0839f80d82982e78d685ab1b2b6
SakineMazlomyar <mazlomyar12@gmail.com>
2018-11-28 21:48:49 +0100

    functionDownUp

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var pos = {x:0 , y: 0}
-var speed = {x: 0, y: 0}
+var speed={x: 0, y:0}
 var picture;
 var div
 var toRightMovment;
 var toLefttMovment;
+var toUpMovment; 
+var toDownMovment;
 
 function init(){
    createSnakeForm()
@@ -24,7 +26,7 @@ function createSnakeForm(){
     document.body.appendChild(div)
 
     //create initial movment
-    toRightMovment = setInterval(goToRight, 300)
+    toRightMovment = setInterval(goToRight, 50)
 }
 
 function directions(){
@@ -35,40 +37,41 @@ function directions(){
             // up arrow
         }
         else if (key.keyCode == '40') {
-            div.style.transform = "scaleX(-1)"
+            //div.style.transform = "scaleY(-1)"
+            clearInterval(toRightMovment);
+            toDownMovment = setInterval(goToDown, 30)
             // down arrow
         }
         else if (key.keyCode == '37') {
             div.style.transform = "scaleX(-1)"
             clearInterval(toRightMovment);
-            toLefttMovment = setInterval(gotToLeft, 300)
+            toLefttMovment = setInterval(gotToLeft, 30)
            // left arrow
         }
         else if (key.keyCode == '39') {
             div.style.transform = "scaleX(1)"
             clearInterval(toLefttMovment);
-            toRightMovment = setInterval(goToRight, 300)
+            toRightMovment = setInterval(goToRight, 30)
            // right arrow
         }
     }  
 }
 
 function goToRight(){
-    speed.x += ((Math.random()*1/100));
-
-    if (pos.x > 50) {
-        alert("Failed ;(")
+    speed.x += ((Math.random()*1/1000));
+    if (pos.x > 89) {
+        console.log("Failed ;(")
         clearInterval(toRightMovment);
     } else {
         pos.x+=speed.x
         div.style.left= pos.x + '%' ; 
         
-        
+ 
     }
 }
 function gotToLeft(){
-    if (pos.x == 0) {
-        alert("Failed ;(")
+    if (pos.x < 0) {
+        console.log("Failed ;(")
         clearInterval(toLefttMovment);
     } else {
         pos.x-=speed.x; 
@@ -77,3 +80,28 @@ function gotToLeft(){
         
     }
 }
+function goToDown(){
+    speed.x += ((Math.random()*1/1000));
+    if (pos.y >100) {
+        console.log("Failed ;(")
+        clearInterval(toDownMovment);
+    } else {
+    
+        div.style.left = pos.x + '%' ; 
+        pos.y+=speed.x 
+        div.style.bottom = pos.y + '%' ; 
+      
+        
+    }
+}
+/* function goToUp(){
+    if (pos.y < 0) {
+        console.log("Failed ;(")
+        clearInterval(toUpMovment);
+    } else {
+        pos.y+=speed.x; 
+        div.style.bottom= pos.y + '%' ; 
+      
+        
+    }
+} */
